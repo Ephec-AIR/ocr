af26ea89611496438b4f016481e1b8425f665402
superuserC <kevin.danthine@gmail.com>
2017-11-23 16:31:47 +0100

    Filtering method modification

--- ocr.py
+++ ocr.py
@@ -6,37 +6,41 @@ import pytesseract
 import time
 
 
-imgB=cv2.imread('img02.jpg')
+imgB=cv2.imread('C:/Users/Kevin/Desktop/img05.png')
 grayscaled = cv2.cvtColor(imgB,cv2.COLOR_BGR2GRAY)
 
 #filtering image
-th = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
-retval2,th3 = cv2.threshold(grayscaled,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+th = cv2.adaptiveThreshold(grayscaled, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,115,1)
 th2 = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,1)
+
+
+th=cv2.bitwise_not(th);
+th2=cv2.bitwise_not(th2);
+th3=cv2.bitwise_not(th3);
 
 #showing filtered images
 cv2.imshow('original',imgB)
-cv2.imshow('Adaptative trh.',th)
-cv2.imshow('Adaptative trh. other param',th2)
-cv2.imshow('OTSU threshold',th3)
+cv2.imshow('th.jpg',th)
+cv2.imshow('th2.jpg',th2)
+cv2.imshow('th3.jpg',th3)
 
 #save temporarely filtered image on disk
 cv2.imwrite('th.jpg',th)
 cv2.imwrite('th2.jpg',th2)
 cv2.imwrite('th3.jpg',th3)
 
-for imgpath in ["th.jpg", "th2.jpg", "th3.jpg"]:
+#apply ocr function on each image
+for imgpath in ["C:/Users/Kevin/Desktop/img05.png","th.jpg", "th2.jpg", "th3.jpg"]:
     imgfile = Image.open(imgpath)
-    imgtext = pytesseract.image_to_string(imgfile)
+    imgtext = pytesseract.image_to_string(imgfile,config='-psm 8 --user-patterns C:\Program Files (x86)\Tesseract-OCR\tessdata\eng.user-patterns --user-words C:\Program Files (x86)\Tesseract-OCR\tessdata\eng.user-words bazaar')
+    print(imgpath)
     print(imgtext)
 
-#apply OCR on filt
 
-time.sleep(5)
 #remove filtered images from the disk
-#os.remove('th.jpg')
-#os.remove('th2.jpg')
-#os.remove('th3.jpg')
+for imgpath in ["th.jpg", "th2.jpg", "th3.jpg"]:
+    os.remove(imgpath)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
