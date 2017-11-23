1de535922210573c260a3ed55d0ba11b618fe963
Julien00859 <julien.castiaux@gmail.com>
2017-11-23 16:46:10 +0100

    forgot to upload file, see commit b4c71c46e09490c347a205e499272ee7f1cd7c06

--- capture.py
+++ capture.py
@@ -1,50 +1,21 @@
-#!./venv/bin/python
+"""Wrapper around raspistill"""
 
-from argparse import ArgumentParser
-from json import dumps as json_dumps
-from sys import argv
-from requests import post as httppost
+from subprocess import run as subproc_run
+from datetime import datetime
+from pathlib import Path
+from contextlib import contextmanager
+from os import remove
+from functools import partial
 
+def capture(filepath):
+    """Capture a image to the given path"""
+    subproc_run(["raspistill", "-o", str(filepath)])
 
-def get_consumption():
-    """Read the video stream and process it to return the consumption."""
-    raise NotImplementedError()
-
-
-def send_consumption(serial, token, consumption):
-    """
-    Feed the web api.
-    
-    Args:
-        serial: OCR public serial
-        token: OCR private token
-        consumption: Measured consumption
-    """
-    httppost("https://air.ephec-ti.org/api/v1/",
-             headers={
-                 "content-type": "application/json"
-             },
-             data=json_dumps({
-                 "serial": serial,
-                 "token": token,
-                 "consumption": consumption
-             }))
-
-
-if __name__ == "__main__":
-    # Define CLI options
-    parser = ArgumentParser(description="Reads the video stream, process it and upload the value to our centralized server where it is saved.")
-    parser.add_argument("-s", "--serial", action="store", help="OCR serial code. Needed to feed the web api")
-    parser.add_argument("-t", "--token", action="store", help="OCR secret token. Needed to feed the web api")
-    parser.add_argument("-c", "--consumption", action="store", type=int, help="Use this value instead for the one given by the OCR.")
-
-    # Parse the args
-    cli = parser.parse_args(argv[1:])
-
-    if cli.serial and cli.token:
-        # Feeds web api
-        send_consumption(cli.serial, cli.token, cli.consumption or get_consumption())
-
-    else:
-        # Feeds stdout
-        print(cli.consumption or get_consumption())
+@contextmanager
+def temp_capture():
+    """Capture a temporary image, yield the path of the generated file"""
+    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.jpg")
+    filepath = Path(Path.home(), "Pictures", filename)
+    capture(filepath)
+    yield filepath
+    remove(filepath)
