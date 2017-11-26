23d988b1430cd61f60b66ccd6338a636c58f8967
Julien00859 <Julien.castiaux@gmail.com>
2017-11-26 16:17:18 +0100

    pep8

--- ocr.py
+++ ocr.py
@@ -1,28 +1,26 @@
+from os import remove
 from PIL import Image
-import os
 import cv2
 import pytesseract
 
-def get_consumption(originalImagePath):
-    filteredImagePath='filteredImage.jpg'
-    
-    originalImage = cv2.imread(originalImagePath)
-    grayscaled = cv2.cvtColor(originalImage,cv2.COLOR_BGR2GRAY)
+def get_consumption(original_image_path):
+    filtered_image_path = 'filteredImage.jpg'
+
+    original_image = cv2.imread(original_image_path)
+    grayscaled = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
     #filtering image
-    filteredImage = cv2.adaptiveThreshold(grayscaled, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,115,1)
-    filteredImage = cv2.bitwise_not(filteredImage);
-     
+    filtered_image = cv2.adaptiveThreshold(grayscaled, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,115,1)
+    filtered_image = cv2.bitwise_not(filtered_image)
+
     #save temporarely filtered image on disk
-    cv2.imwrite(filteredImagePath,filteredImage)
-    
+    cv2.imwrite(filtered_image_path, filtered_image)
+
     #apply ocr function on filtered image
-    imgfile = Image.open(filteredImagePath)
-    imgtext = pytesseract.image_to_string(imgfile,config='-psm 6 digits')    
-    
+    imgfile = Image.open(filtered_image_path)
+    imgtext = pytesseract.image_to_string(imgfile, config='-psm 6 digits')    
+
     #remove filtered image from the disk
-    os.remove(filteredImagePath)
-    
+    remove(filtered_image_path)
+
     return int(imgtext.replace(" ", ""))
-
-
