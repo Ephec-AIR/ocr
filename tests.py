b14dfacbd5dcda224f7c2658f5f351573c102f40
Julien00859 <Julien.castiaux@gmail.com>
2017-11-26 19:58:40 +0100

    translate folder name in english

--- tests.py
+++ tests.py
@@ -7,51 +7,51 @@ from ocr import get_consumption
 
 class Test(unittest.TestCase):
     def test_meter_01(self):
-        imgpath = str(Path("compteurs") / "01.jpg")
+        imgpath = str(Path("meters") / "01.jpg")
         self.assertEqual(get_consumption(imgpath), 23355)
 
     def test_meter_02(self):
-        imgpath = str(Path("compteurs") / "02.jpg")
+        imgpath = str(Path("meters") / "02.jpg")
         self.assertEqual(get_consumption(imgpath), 4998)
 
     def test_meter_03(self):
-        imgpath = str(Path("compteurs") / "03.jpg")
+        imgpath = str(Path("meters") / "03.jpg")
         self.assertEqual(get_consumption(imgpath), 1)
 
     def test_meter_04(self):
-        imgpath = str(Path("compteurs") / "04.jpg")
+        imgpath = str(Path("meters") / "04.jpg")
         self.assertEqual(get_consumption(imgpath), 18364)
 
     def test_meter_05(self):
-        imgpath = str(Path("compteurs") / "05.jpg")
+        imgpath = str(Path("meters") / "05.jpg")
         self.assertEqual(get_consumption(imgpath), 1820)
 
     def test_meter_06(self):
-        imgpath = str(Path("compteurs") / "06.jpg")
+        imgpath = str(Path("meters") / "06.jpg")
         self.assertEqual(get_consumption(imgpath), 27)
 
     def test_meter_07(self):
-        imgpath = str(Path("compteurs") / "07.jpg")
+        imgpath = str(Path("meters") / "07.jpg")
         self.assertEqual(get_consumption(imgpath), 72546)
 
     def test_meter_08(self):
-        imgpath = str(Path("compteurs") / "08.jpg")
+        imgpath = str(Path("meters") / "08.jpg")
         self.assertEqual(get_consumption(imgpath), 70843)
 
     def test_meter_09(self):
-        imgpath = str(Path("compteurs") / "09.jpg")
+        imgpath = str(Path("meters") / "09.jpg")
         self.assertEqual(get_consumption(imgpath), 70107.9)
 
     def test_meter_10(self):
-        imgpath = str(Path("compteurs") / "10.jpg")
+        imgpath = str(Path("meters") / "10.jpg")
         self.assertEqual(get_consumption(imgpath), 0.01)
 
     def test_meter_11(self):
-        imgpath = str(Path("compteurs") / "11.jpg")
+        imgpath = str(Path("meters") / "11.jpg")
         self.assertEqual(get_consumption(imgpath), 13131.3)
 
     def test_meter_12(self):
-        imgpath = str(Path("compteurs") / "12.jpg")
+        imgpath = str(Path("meters") / "12.jpg")
         self.assertEqual(get_consumption(imgpath), 85407)
 
 
