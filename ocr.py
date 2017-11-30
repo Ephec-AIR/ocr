d349e90a351e017deeaac7524af25e95c85e76f2
superuserC <kevin.danthine@gmail.com>
2017-11-30 10:45:17 +0100

    Improve image filter before ocr application

--- ocr.py
+++ ocr.py
@@ -4,9 +4,16 @@ import cv2
 import pytesseract
 
 def get_consumption(original_image_path):
-    filtered_image_path = 'filteredImage.jpg'
 
-    original_image = cv2.imread(original_image_path)
+    resized_image_path = 'compteurs_opti/resizedImage.jpg'
+    filtered_image_path = 'compteurs_opti/filteredImage.jpg'
+
+    original_image_big = Image.open(original_image_path)
+    original_image_big.thumbnail((200,200), Image.ANTIALIAS)
+    original_image_big = original_image_big.crop((20,50,180,100))
+    original_image_big.save(resized_image_path)
+    
+    original_image = cv2.imread(resized_image_path)
     grayscaled = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
     #filtering image
@@ -15,12 +22,9 @@ def get_consumption(original_image_path):
 
     #save temporarely filtered image on disk
     cv2.imwrite(filtered_image_path, filtered_image)
-
+    
     #apply ocr function on filtered image
     imgfile = Image.open(filtered_image_path)
     imgtext = pytesseract.image_to_string(imgfile, config='-psm 6 digits')    
-
-    #remove filtered image from the disk
-    remove(filtered_image_path)
 
     return int(imgtext.replace(" ", ""))
