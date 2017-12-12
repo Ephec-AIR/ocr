67da0c67974ed7364c8d91c06878fe8aa59cef33
Julien00859 <julien.castiaux@gmail.com>
2017-12-12 10:35:42 +0100

    add thresh argument in cli (see ocr.binarize_array)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!./venv/bin/python
 
 from argparse import ArgumentParser
+from functools import partial
 from sys import argv
 from capture import temp_capture
 from ocr import get_consumption
@@ -10,12 +11,17 @@ def main():
     # Define CLI arguments
     parser = ArgumentParser(description="Reads the video stream, process it and post the value to the webapi.")
     parser.add_argument("-f", "--file", action="store", help="File to read image from. If not given, use raspistill to capture a new one using the camera")
+    parser.add_argument("--thresh", action="store", type=int, help="Threshold used in the binarization for the image cleanup")
     parser.add_argument("-c", "--consumption", action="store", type=int, help="Consumption to post to web api. If not given, read it using a ocr")
     parser.add_argument("-s", "--serial", action="store", help="Serial code used along the POST request. If not given, print on stdout.")
     parser.add_argument("-t", "--token", action="store", help="OCR Secret code used along the POST request. If not given, print on stdout.")
 
     # Parse the args
     cli = parser.parse_args(argv[1:])
+
+    # If a threshold is set, patch get_consumption 
+    if cli.thresh:
+        get_consumption = partial(get_consumption, threshold=cli.thresh)
 
     # Get consumption from CLI or from OCR.
     if cli.consumption is not None:
